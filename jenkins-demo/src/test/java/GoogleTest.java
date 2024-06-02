45ee4f8c8609c7162c2782f18134d807c246de3f
archimedesAJ <abrahamabbeyjr456@gmail.com>
2024-06-02 23:18:20 +0000

    gut

--- jenkins-demo/src/test/java/GoogleTest.java
+++ jenkins-demo/src/test/java/GoogleTest.java
@@ -1,4 +1,7 @@
+import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -7,11 +10,17 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class GoogleTest {
+    ExtentReports extent = new ExtentReports();
+    ExtentSparkReporter spark = new ExtentSparkReporter("target/Report.html");
+
     private WebDriver driver;
 
     //Setting up the page
     @BeforeClass
     public void setUp(){
+        spark.config().setTheme(Theme.DARK);
+        spark.config().setDocumentTitle("My Report");
+        extent.attachReporter(spark);
 
         WebDriverManager.firefoxdriver().setup();
         driver = new FirefoxDriver();
@@ -20,6 +29,7 @@ public class GoogleTest {
 
     @AfterTest
     public void teardown(){
+        extent.flush();
         driver.quit();
 
     }
@@ -27,13 +37,14 @@ public class GoogleTest {
     //Test to open the site
     @Test
     public void OpenSite() throws InterruptedException {
+        ExtentTest test = extent.createTest("Confirm Page Title").assignAuthor("Abraham").assignCategory("Functional Test").assignDevice("Windows");
         driver.get("https://google.com/");
         String page_title = driver.getTitle();
         if (page_title.equals("Google")){
-            System.out.println("passed");
+            test.pass("Test passed. Page titles match!");
         }
         else {
-            System.out.println("failed");
+            test.fail("Test failed. Page titles mismatch!");
         }
 
         Thread.sleep(2000);
